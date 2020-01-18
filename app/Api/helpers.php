a15b063876ceb0f2f4290b7e80aa3a230b420584
欧阳文教 <wenjiao8605@126.com>
2020-01-18 11:02:23 +0800

    公共函数库加载

--- app/Api/helpers.php
+++ app/Api/helpers.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\DB;
 
 /**
  * 获取时间
@@ -10,4 +11,25 @@ function timestamp($time = 0) {
         return date('Y-m-d H:i:s', $time);
     }
     return date('Y-m-d H:i:s');
+}
+
+/**
+ * 调试SQL语句
+ */
+function getLastSql() {
+    DB::listen(function ($sql) {
+        foreach ($sql->bindings as $i => $binding) {
+            if ($binding instanceof \DateTime) {
+                $sql->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+            } else {
+                if (is_string($binding)) {
+                    $sql->bindings[$i] = "'$binding'";
+                }
+            }
+        }
+        $query = str_replace(array('%', '?'), array('%%', '%s'), $sql->sql);
+        $query = vsprintf($query, $sql->bindings);
+        print_r($query);
+        echo '<br />';
+    });
 }
